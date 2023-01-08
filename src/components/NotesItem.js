25e7c8e1958f2daca6151be03efa7306de2580f4
Abdullah4567 <gemstones321@gmail.com>
2023-01-08 20:50:52 +0500

    markAsImportant + markAs Ordinary added

--- src/components/NotesItem.js
+++ src/components/NotesItem.js
@@ -2,13 +2,13 @@ import React, { useContext } from 'react'
 import Context from '../context/notes/NoteContext'
 const NotesItem = (props) => {
     const notesContext = useContext(Context)
-    const { deleteNote } = notesContext;
+    const { deleteNote, markAsImportant, markAsOrdinary } = notesContext;
     const deleteStyle = {
-        fontSize: "x-large",
+        fontSize: "large",
         color: "crimson",
     }
     const editStyle = {
-        fontSize: "x-large",
+        fontSize: "large",
         color: "green",
     }
     return (
@@ -17,12 +17,20 @@ const NotesItem = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
-                    <hr />
+                    <hr className='m-2' />
                     <div className="d-flex justify-content-around">
                         <i className="fa-solid fa-trash" style={deleteStyle} onClick={() => {
                             deleteNote(props.id)
                         }}></i>
                         <i className="fa-regular fa-pen-to-square" style={editStyle}></i>
+                    </div>
+                    <div className="d-flex justify-content-around mt-1">
+                        <p className="fw-light fs-6 mt-1 ps-1" style={{ marginBottom: "1px" }}>Mark as important </p>
+                        <div className="form-check form-switch  mt-2">
+                            <input className="form-check-input" type="checkbox" id="markAsImportant" onClick={(e) => {
+                                e.target.checked ? markAsImportant(props.id) : markAsOrdinary(props.id)
+                            }} />
+                        </div>
                     </div>
                 </div>
             </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -13,56 +13,56 @@ const NoteState = (props) => {
     {
         "_id": "63b5393675fe3a1ec7a3122",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is second title",
+        "description": "This is second description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     },
     {
         "_id": "63b539675fe3a1ec7a312b2",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is third title",
+        "description": "This is third description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     },
     {
         "_id": "63b5393675fe3a1e7a312b2",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is 4th title",
+        "description": "This is 4th description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     },
     {
         "_id": "63b5393675fe3a1ec7a32b2",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is 5th title",
+        "description": "This is 5th description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     },
     {
         "_id": "63b539375fe3a1ec7a312b2",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is 6th title",
+        "description": "This is 6th description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     },
     {
         "_id": "63b5393675f3a1ec7a312b2",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is 7th title",
+        "description": "This is fir7thst description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     },
     {
         "_id": "63b593675fe3a1ec7a312b2",
         "userId": "63b5356dd7960e4f20b3bab6",
-        "title": "This is first title",
-        "description": "This is first description",
+        "title": "This is 8th title",
+        "description": "This is 8th description",
         "createdAt": "2023-01-04T08:30:46.044Z",
         "__v": 0
     }]
@@ -82,13 +82,33 @@ const NoteState = (props) => {
     const deleteNote = (id) => {
         console.log(id);
         const newNotes = notes.filter((note) => {
-            return note._id != id
+            return note._id !== id
         })
         setnotes(newNotes)
 
     }
+    const markAsImportant = (id) => {
+        const newNotes = notes.filter((note) => {
+            return note._id !== id
+        })
+        const importantNote = notes.find((note) => {
+            return note._id === id
+        })
+        newNotes.unshift(importantNote);
+        setnotes(newNotes);
+    }
+    const markAsOrdinary = (id) => {
+        const newNotes = notes.filter((note) => {
+            return note._id !== id
+        })
+        const importantNote = notes.find((note) => {
+            return note._id === id
+        })
+        newNotes.push(importantNote);
+        setnotes(newNotes);
+    }
     return (
-        <NoteContext.Provider value={{ notes, addNewNote, deleteNote }}>
+        <NoteContext.Provider value={{ notes, addNewNote, deleteNote, markAsImportant, markAsOrdinary }}>
             {props.children}
         </NoteContext.Provider >
     )
